5d6fd64f5c09cc49e92cf99094c23c7491db9e09
Matteo Pagani <info@qmatteoq.com>
2017-11-10 20:03:34 +0100

    First commit of the hololens project

---
+++ Assets/Scripts/PositionManager.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class PositionManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        if (OpaqueDetector.IsOpaque)
+        {
+            this.gameObject.transform.position = new Vector3(0f, 1f, 1.8f);
+        }
+        else
+        {
+            this.gameObject.transform.position = new Vector3(0f, -0.5f, 1.8f);
+        }
+    }
+}
