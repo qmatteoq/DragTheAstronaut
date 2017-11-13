d58e2ffee2b7cbd5e031d299ee91aa7f93bdab38
Matteo Pagani <info@qmatteoq.com>
2017-11-13 16:18:35 +0100

    Updated .gitignore to remove build artifacts

---
+++ Assets/Scripts/GazeRotator.cs
@@ -0,0 +1,27 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeRotator : MonoBehaviour, IFocusable
+{
+    private bool isRotating;
+
+    public void OnFocusEnter()
+    {
+        isRotating = true;
+    }
+
+    public void OnFocusExit()
+    {
+        isRotating = false;
+    }
+
+    void Update()
+    {
+        if (isRotating)
+        {
+            transform.Rotate(Vector3.up, 30 * Time.deltaTime);
+        }
+    }
+}
